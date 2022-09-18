30f6627730f69d9cd3a36e22135be53bec9d49f1
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2022-09-18 17:25:13 +0530

    Update q3.c

--- q3.c
+++ q3.c
@@ -1,36 +1,46 @@
-// Regular Falsi
-
-// 0 1, 0.001
-
 #include<stdio.h>
 #include<math.h>
 
-float f(float x){
-  return (x*x*x)-(5*x)+1;
-  }
-  
- int main()
- {
-  float a,b,err;
-  printf("enter value of a and b");
-  scanf("%f%f",&a,&b);
-  if(f(a)*f(b)<0){
-    scanf("%f",&err);
-    float x=a-((b-a)/(f(a)-f(b)))*f(a);
-    while(fabs(f(x))>err)
-    {
-      if(f(a)*f(x)<0)
-      b=x;
-      else if(f(x)*f(b)<0){
-      a=x;}
-      x=a-((b-a)/(f(a)-f(b)))*f(a);
-      printf("x = %f f(x) = %f\n",fabs(x),f(x));}
-      printf(" answer x = %f \n",fabs(x));
-    }
-  else{
-    printf("Range is incorrect \n");
-  }
-    
-    return 0;
-
+float fx(float x)
+{
+   return((3*x)+sin(x)-exp(x));
 }
+
+int main()
+{
+   printf("For Equation: 3x+sin(x)-e^x \n");
+   
+   float a,b,er;
+   int it=1;
+   
+   printf("Enter the values of a and b: \n");
+   scanf("%f %f", &a, &b);
+   
+   if(fx(a)*fx(b)<0)
+   {
+      printf("Roots found at %0.3f and %0.3f \n",a,b);
+      printf("Enter the allowed error: ");
+      scanf("%f",&er);
+      
+      float x= a-((b-a)/(fx(b)-fx(a)))*fx(a);
+      
+      while(fabs(fx(x))>er)
+      {
+        x= a-((b-a)/(fx(b)-fx(a)))*fx(a);
+        printf("%d Iteration for %f and %f, the value x is: %f \n",it,a,b,x);
+        
+        if(fabs(fx(x))<er)
+           printf("Roots Found at: %f",x);
+        else if(fx(a)*fx(x)<0)
+           b=x;
+        else if(fx(x)*fx(b)<0)
+           a=x;
+        it++;
+      }
+   }
+   else
+   {
+     printf("Wrong Intervall try again!! \n");
+   }
+   return 0;
+ }
