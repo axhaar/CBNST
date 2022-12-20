345d0eae05a588e1c9d7a4df64ef1074e51f8ea0
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2022-12-21 00:40:39 +0530

    Linear Regression

--- q18.c
+++ q18.c
@@ -1,40 +1,21 @@
+#include<math.h>
 #include<stdio.h>
-#include<conio.h>
-
-#define S 50
-
-int main()
-{
-    printf ("Achintya Mishra \t A \t 20011857\n"); 
-
- int n, i;
- float x[S], y[S], sumX=0, sumX2=0, sumY=0, sumXY=0, a, b;
- 
- /* Input */
- printf("\nEnter the number of points: ");
- scanf("%d", &n);
- printf("\nEnter values of x: ");
- for(i=1;i<=n;i++)
- scanf("%f", &x[i]);
-
-printf ("\nEnter values of y: "); 
-for(i=1;i<=n;i++)
-scanf("%f", &y[i]);
- 
- /* Calculating Required Sum */
- for(i=1;i<=n;i++)
- {
-  sumX = sumX + x[i];
-  sumX2 = sumX2 + x[i]*x[i];
-  sumY = sumY + y[i];
-  sumXY = sumXY + x[i]*y[i];
- }
- /* Calculating a and b */
- b = (n*sumXY-sumX*sumY)/(n*sumX2-sumX*sumX);
- a = (sumY - b*sumX)/n;
- /* Displaying value of a and b */
- printf("A=%0.2f and B = %0.2f\n",a,b);
- printf("\nEquation: y = %0.2f + %0.2fx",a,b);
-
- return(0);
+main(){
+   int n,i;
+   float x,y,m,c,d;
+   float sumx=0,sumxsq=0,sumy=0,sumxy=0;
+   printf("enter the number of values for n:");
+   scanf("%d",&n);
+   for(i=0;i<n;i++){
+      printf("enter values of x and y");
+      scanf("%f%f",&x,&y);
+      sumx=sumx+x;
+      sumxsq=sumxsq+(x*x);
+      sumy=sumy+y;
+      sumxy=sumxy+(x*y);
+   }
+   d=n*sumxsq-sumx*sumx;
+   m=(n*sumxy-sumx*sumy)/d;
+   c=(sumy*sumxsq-sumx*sumxy)/d;
+   printf("M=%f\tC=%f",m,c);
 }
